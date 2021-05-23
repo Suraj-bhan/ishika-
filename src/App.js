1981ee89b8d8dc1576dbaa8aeb35fef5362e2127
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-05-24 02:33:15 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>React App</h1>
          <p>Ishika is beautiful.HOT............</p>
-         <p>Ishika is beautiful.HOT............</p>
-         <p>Ishika is beautiful.HOT............</p>
+
       </header>
     </div>
   );
